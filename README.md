2a4ad49b8bea674e4b8734eefa8c0973ececd293
Sameer Yadav <sameer.yadv8@gmail.com>
2023-09-16 22:11:31 +0530

    added readCsvAsync function

--- README.md
+++ README.md
@@ -13,9 +13,13 @@ npm install pandas-csv-node
 ## API
 
 ```js
-import {readCsv} from "pandas-csv-node"
+import {readCsv,readCsvAsync} from "pandas-csv-node"
 ```
 
 ### readCsv
 
 `readCsv` takes two argument one is `filePath` and other is `encoding`, by default encoding is set to `utf-8`.
+
+### readCsvAsync
+
+`readCsvAsync` takes same arguments as `readCsv` but this is async function which return a promise which resolves to array of data.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { readFileSync } from "fs";
+import { readFileSync,readFile } from "fs";
 
 /**
  * This function will return an object with keys from header of csv and values as array
@@ -22,4 +22,33 @@ export const readCsv = (filePath,encoding) => {
         })
     })
     return result
+}
+
+/**
+ * This function will return a promise which resolves to an object with keys from header of csv and values as array
+ * @param {string} filePath Path of the csv file
+ * @param {string} [encoding = 'utf-8'] - Encoding of the file 
+ * @returns {Object} Object containing key value pair where keys are header of csv and values are array
+ */
+export const readCsvAsync = async (filePath,encoding) => {
+    const result = {}
+    readFile(filePath,{encoding:encoding||'utf-8'},(err,data) => {
+        if (err) {
+            console.error(err)
+            return err;
+        }
+        const dataRow = data.split('\r\n')
+        const cell = []
+        dataRow.forEach(dt => cell.push(dt.split(',')))
+        cell[0].forEach(el => result[el] = [])
+        cell.forEach((data,index) => {
+            if (index==0) {
+            return
+        }
+        data.forEach((data,index) => {
+            result[cell[0][index]].push(data)
+        })
+    })
+    return result;
+    })
 }
